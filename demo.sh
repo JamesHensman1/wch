aff7230d19052e47e979176d6286f8c5c39adf0d
JamesHensman1 <jameshensman123@gmail.com>
2022-05-17 15:27:43 +0100

    Revert "Revert "First Version, takes in a file and acts like the command wc on unix systems""
    This reverts commit 956f84fe1612e764b8467089a50e1b2c4c1e2b51.

---
+++ demo.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+if [ -z $(which cabal) ]; then
+  echo "Please install cabal https://www.haskell.org/ghcup/"
+	exit
+fi
+
+if [ ! -f "wch" ]; then
+  cabal build
+	cp dist/build/wch/wch .
+fi
+
+fname="test.txt"
+if [ ! -z "$1" ]; then
+  fname=$1
+fi
+
+echo  "Using file $fname"
+echo "This is the regular wc command"
+wc $fname
+echo ""
+echo "This is the haskell verion"
+./wch $fname
+
